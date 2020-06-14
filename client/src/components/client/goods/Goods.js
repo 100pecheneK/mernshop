c66e6168ca880f40eda58fd34baa7ae79b6b744e
100pecheneK <mistermihail23@gmail.com>
2020-06-14 17:02:38 +0500

    Add Lazy load img

--- client/src/components/client/goods/Goods.js
+++ client/src/components/client/goods/Goods.js
@@ -1,44 +1,58 @@
-import React, {useEffect} from 'react'
+import React, { useEffect } from 'react'
 import PropTypes from 'prop-types'
-import {connect} from "react-redux"
-import SpinnerLinear from "../../layout/SpinnerLinear"
+import { connect } from 'react-redux'
+import SpinnerLinear from '../../layout/SpinnerLinear'
 import alert from '../../../utils/alert'
-import Pagination from "../../layout/Pagination"
-import {getGoods} from "../../../actions/admin/goods"
-import {addItemToCart} from "../../../actions/client/cart"
-
+import Pagination from '../../layout/Pagination'
+import { getGoods } from '../../../actions/admin/goods'
+import { addItemToCart } from '../../../actions/client/cart'
+import ImageLoader from './ImageLoader/ImageLoader'
+import LazyLoad from 'react-lazy-load'
 const Goods = ({
-                 getGoods,
-                 addItemToCart,
-                 goods: {loading, goods: {docs, totalPages, page, limit}},
-                 match
-               }) => {
+  getGoods,
+  addItemToCart,
+  goods: {
+    loading,
+    goods: { docs, totalPages, page, limit },
+  },
+  match,
+}) => {
   useEffect(() => {
     getGoods(match.params.page)
   }, [getGoods, match.params.page])
-  const onGoodAddToCartClick = doc => {
-    addItemToCart({id: doc._id, name: doc.name, price: doc.price})
+  const onGoodAddToCartClick = (doc) => {
+    addItemToCart({ id: doc._id, name: doc.name, price: doc.price })
     alert('Товар добавлен', 'green')
   }
 
-  return loading ? <SpinnerLinear/> : (
+  return loading ? (
+    <SpinnerLinear />
+  ) : (
     <>
       <div className="container">
         <div className="row">
-          <div className='col'>
+          <div className="col">
             <div className="card-columns">
-
               {docs.map((doc, i) => {
                 return (
                   <div className="card" key={doc._id}>
                     <div className="card-image">
-                      <img
-                        alt="good"
-                        src={`/${doc.images[0]}`}/>
+                      <LazyLoad
+                        key={i}
+                        debounce={false}
+                        throttle={250}
+                        offsetVertical={300}
+                      >
+                        <ImageLoader alt="good" src={`/${doc.images[0]}`} />
+                      </LazyLoad>
                       <span className="card-title">{doc.name}</span>
-                      <a href="#!" onClick={() => onGoodAddToCartClick(doc)}
-                         className="btn-floating halfway-fab waves-effect waves-light red">
-                        <i className="material-icons">add_shopping_cart</i></a>
+                      <a
+                        href="#!"
+                        onClick={() => onGoodAddToCartClick(doc)}
+                        className="btn-floating halfway-fab waves-effect waves-light red"
+                      >
+                        <i className="material-icons">add_shopping_cart</i>
+                      </a>
                     </div>
                     <div className="card-content">
                       <b>{doc.category.name}</b>
@@ -70,10 +84,7 @@ Goods.propTypes = {
   addItemToCart: PropTypes.func.isRequired,
   goods: PropTypes.object.isRequired,
 }
-const mapStateToProps = state => ({
-  goods: state.goods
+const mapStateToProps = (state) => ({
+  goods: state.goods,
 })
-export default connect(
-  mapStateToProps,
-  {getGoods, addItemToCart}
-)(Goods)
+export default connect(mapStateToProps, { getGoods, addItemToCart })(Goods)
